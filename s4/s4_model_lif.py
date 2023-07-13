9adf365c09d5baf4b3968c6a12e22fb6cf5e23d7
dyabel <abelazady@foxmail.com>
2023-07-13 14:06:07 +0800

    1

--- s4/s4_model_lif.py
+++ s4/s4_model_lif.py
@@ -14,7 +14,8 @@ from s4.s4d import S4D
 from tqdm.auto import tqdm
 import torch.nn.functional as F
 from lava.lib.dl import slayer
-
+from lava.proc.lif.process import LIF
+import lava.lib.dl.bootstrap as bootstrap
 
 # Dropout broke in PyTorch 1.11
 if tuple(map(int, torch.__version__.split('.')[:2])) == (1, 11):
@@ -56,7 +57,6 @@ class S4Model(nn.Module):
         # self.input =  slayer.block.sigma_delta.Input(sdnn_params)
         # self.input.pre_hook_fx = self.input_quantizer
         # Linear encoder (d_input = 1 for grayscale and 3 for RGB)
-        self.encoder = slayer.block.sigma_delta.Dense(sdnn_params, d_input, d_model, weight_norm=False, delay=True, delay_shift=False)
         # Stack S4 layers as residual blocks
         self.s4_layers = nn.ModuleList()
         self.norms = nn.ModuleList()
@@ -68,13 +68,32 @@ class S4Model(nn.Module):
             self.norms.append(nn.LayerNorm(d_model))
             self.dropouts.append(dropout_fn(dropout))
 
+
+        neuron_params = {
+                'threshold'     : 1.25,
+                'current_decay' : 1, # this must be 1 to use batchnorm
+                'voltage_decay' : 0.03,
+                'tau_grad'      : 1,
+                'scale_grad'    : 1,
+            }
+        neuron_params_norm = {
+                **neuron_params, 
+                # 'norm'    : slayer.neuron.norm.MeanOnlyBatchNorm,
+            }
+        
+        self.encoder = bootstrap.block.cuba.Dense(neuron_params_norm, d_input, d_model, weight_norm=True, weight_scale=2)
+        # self.blocks = torch.nn.ModuleList([
+                # bootstrap.block.cuba.Input(neuron_params, weight=1, bias=0), # enable affine transform at input
+                # bootstrap.block.cuba.Dense(neuron_params_norm, 28*28, 512, weight_norm=True, weight_scale=2),
+                # bootstrap.block.cuba.Dense(neuron_params_norm, 512, 512, weight_norm=True, weight_scale=2),
+                # bootstrap.block.cuba.Affine(neuron_params, 512, 10, weight_norm=True, weight_scale=2),
+            # ])
+
+        
         # Linear decoder
         # self.decoder = nn.Linear(d_model, d_output)
         # self.decoder =  slayer.block.sigma_delta.Output(sdnn_params, d_model, d_output, weight_norm=False)
-        max_delay = 0
-        self.decoder = slayer.block.sigma_delta.Dense(sdnn_params, d_model, d_output, weight_norm=False, delay=True, delay_shift=False)
-        self.encoder.delay.max_delay = max_delay
-        self.decoder.delay.max_delay = max_delay
+        self.decoder =  bootstrap.block.cuba.Affine(neuron_params, d_model, d_output, weight_norm=True, weight_scale=2)
         # self.output = slayer.block.sigma_delta.Output(sdnn_params, d_model, d_output, weight_norm=False)
 
     def forward(self, x):
@@ -82,8 +101,10 @@ class S4Model(nn.Module):
         Input x is shape (B, L, d_input)
         """
         # x = self.input(x)
-        x = self.encoder(x)  # (B, L, d_input) -> (B, L, d_model)
-        # print(x)
+        # x = x.transpose(-1, -2)  # (B, L, d_model) -> (B, d_model, L)
+        x = self.encoder(x, mode=bootstrap.Mode.SNN)  # (B, L, d_input) -> (B, L, d_model)
+        # print(x.shape)
+        # x = x.transpose(-1, -2)  # (B, L, d_model) -> (B, d_model, L)
 
         # x = x.transpose(-1, -2)  # (B, L, d_model) -> (B, d_model, L)
         for layer, norm, dropout in zip(self.s4_layers, self.norms, self.dropouts):
@@ -113,7 +134,7 @@ class S4Model(nn.Module):
         # x = x.mean(dim=1)
 
         # Decode the outputs
-        x = self.decoder(x)  # (B, d_model) -> (B, d_output)
+        x = self.decoder(x, mode=bootstrap.Mode.ANN)  # (B, d_model) -> (B, d_output)
         # x = self.output(self.decoder(x))  # (B, d_model) -> (B, d_output)
 
         return x
